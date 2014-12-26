45423c79024096ecb59c98fda0d74abfc3aff2c5
420303844@qq.com <admin_123>
2014-12-26 18:02:57 +0800

    bug
    bug

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var http = require('http');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -17,12 +18,14 @@ var db = require('./models/db');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views')); 
 app.set('view engine', 'ejs');
+app.set('view options',{layout:true})
 
 
-app.use(partials())
+app.use(partials());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -30,7 +33,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use(express.cookieParser());
 app.use(session({
@@ -48,6 +51,22 @@ app.use('/test/:username',function(req,res,next){
 });
 app.use('/login',login);
 
+app.use(function(req,res,next){
+    var err = req.session.error,
+    msg = req.session.success;
+    //删除会话中原有属性
+    delete req.session.error;
+    delete req.session.success;
+    res.locals.ms = "提示1";
+    console.log(res.locals.ms);
+    res.locals.message = '';
+    if(err) {res.locals.message = '<div style="color:red">'+err+'</div>';}
+    if(success) {res.locals.message = '<div style="color:blue">'+success+'</div>';}
+    next();
+});
+
+//app.use(app.router);  
+app.use(express.static(path.join(__dirname, 'public')));
 //片段视图
 // app.use('/list',function(req,res){
 //     res.render('list',{
@@ -86,13 +105,13 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+// app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//         message: err.message,
+//         error: {}
+//     });
+// });
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -20,40 +20,42 @@ router.get('/list',function(req,res){
 })
 
 
-/*exports.doReg = function(req,res){
-	if(req.body["password-repeat"]!=req.body['password']){
-		req.session.error="两次输入口令不一致";
-		return res.redirect('/reg');
-	}
 
-	var md5 = crypto.createHash('md5');
-	var password = md5.update(req.body.password).digest('base64');
 
-	var newUser = new User({
-		name:req.body.username,
-		password:password
-	});
+// exports.doReg = function(req,res){
+// 	if(req.body["password-repeat"]!=req.body['password']){
+// 		req.session.error="两次输入口令不一致";
+// 		return res.redirect('/reg');
+// 	}
 
-	User.find(newUser.name,function(err,user){
-		if(user){
-			req.session.error = "该用户已经存在";
-			return res.redirect('/reg');
-		}
+// 	var md5 = crypto.createHash('md5');
+// 	var password = md5.update(req.body.password).digest('base64');
 
-		newUser.save(function(err){
-			if(err){
-				req.session.error = err;
-				return res.redirect('/reg');
-			}
-			req.session.user = newUser;
-			req.session.success = '注册成功';
-			res.locals.user = newUser;
-			res.redirect('/');
-		})
-	})
-}*/
+// 	var newUser = new User({
+// 		name:req.body.username,
+// 		password:password
+// 	});
 
-router.post("/reg", function(req,res){
+// 	User.find(newUser.name,function(err,user){
+// 		if(user){
+// 			req.session.error = "该用户已经存在";
+// 			return res.redirect('/reg');
+// 		}
+
+// 		newUser.save(function(err){
+// 			if(err){
+// 				req.session.error = err;
+// 				return res.redirect('/reg');
+// 			}
+// 			req.session.user = newUser;
+// 			req.session.success = '注册成功';
+// 			res.locals.user = newUser;
+// 			res.redirect('/');
+// 		})
+// 	})
+// }
+
+router.post("/doreg", function(req,res){
 	console.log('用户名：'+req.body.username);
 	console.log('密码：'+req.body["password"]);
 	if(req.body["password-repeat"]!=req.body['password']){
@@ -86,6 +88,8 @@ router.post("/reg", function(req,res){
 			res.redirect('/');
 		})
 	})
-})
+});
+
+
 
 module.exports = router;
